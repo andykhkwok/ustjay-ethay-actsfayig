c18b3e7728d05e78850888465752e698068c8663
Andy Kwok <andykhkwok@gmail.com>
2019-09-02 22:56:02 -0700

    initial commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 
 def get_fact():
@@ -13,15 +14,27 @@ def get_fact():
 
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
-
     return facts[0].getText()
+    
+def get_link(input):
+    
+    params  = { 'input_text': input}
+    
+    link_info = requests.post("http://hidden-journey-62459.herokuapp.com/piglatinize/", data = params , allow_redirects=False)
 
+    result = link_info.headers['Location']
+    
+    hyperlink_format = '<a href="{link}">{text}</a>'
+    hyperlink_format.format(link= result, text='HyperLink to Pig Latin:')    
+
+    return hyperlink_format.format(link= result, text='HyperLink to Pig Latin.') 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
-
-
+    input = get_fact()
+    link = get_link(input)
+    return link
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
